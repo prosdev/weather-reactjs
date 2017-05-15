c417979bdf2af3348c12cbf66f813ecc57f0b618
pseng <pros.seng@healthsparq.com>
2017-05-15 12:53:08 -0700

    Add average line to chart

--- src/containers/WeatherList.jsx
+++ src/containers/WeatherList.jsx
@@ -7,15 +7,16 @@ class WeatherList extends Component {
 
     renderWeather(cityData) {
         const name = cityData.city.name;
-        const temps = cityData.list.map(weather => weather.main.temp);
-        console.log(temps);
+        const temps = cityData.list.map(weather => ((weather.main.temp * 9/5) - 459.67));
+        const pressure = cityData.list.map(weather => weather.main.pressure);
+        const humidity = cityData.list.map(weather => weather.main.humidity);
 
         return (
             <tr key={name}>
                 <td>{name}</td>
-                <td>
-                    <LineChart data={temps} color="orange" />
-                </td>
+                <td><LineChart data={temps} color="orange" units="°F"/></td>
+                <td><LineChart data={pressure} color="green" units="hPa"/></td>
+                <td><LineChart data={humidity} color="black" units="%"/></td>
             </tr>
         )
     }
@@ -26,9 +27,9 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (°F)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>

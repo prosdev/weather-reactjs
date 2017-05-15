f9e332f07adbcc810f1575b51fa6e362d2f08d68
pseng <pros.seng@healthsparq.com>
2017-05-15 11:58:15 -0700

    Implement chart for temperature. Add react-sparklines library

---
+++ src/containers/WeatherList.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import LineChart from '../components/LineChart';
+
+
+class WeatherList extends Component {
+
+    renderWeather(cityData) {
+        const name = cityData.city.name;
+        const temps = cityData.list.map(weather => weather.main.temp);
+        console.log(temps);
+
+        return (
+            <tr key={name}>
+                <td>{name}</td>
+                <td>
+                    <LineChart data={temps} color="orange" />
+                </td>
+            </tr>
+        )
+    }
+
+    render() {
+        return (
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>City</th>
+                        <th>Temperature</th>
+                        <th>Pressure</th>
+                        <th>Humidity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {this.props.weather.map(this.renderWeather)}
+                </tbody>
+            </table>
+        )
+    }
+}
+
+function mapStateToProps({weather}) {
+    return {
+        weather
+    }
+}
+
+export default connect(mapStateToProps)(WeatherList);
